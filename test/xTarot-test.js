390156f168638ea213e0aba9aafa1318856f6e83
Magnus Brantheim <m.brantheim@gmail.com>
2022-01-07 23:26:08 +0000

    Get deposit test to pass

--- test/xTarot-test.js
+++ test/xTarot-test.js
@@ -158,140 +158,143 @@ describe("Vaults", function () {
       expect(pricePerFullShare).to.equal(ethers.utils.parseEther("1"));
     });
   });
-  // xdescribe("Vault Tests", function () {
-  //   xit("should allow deposits and account for them correctly", async function () {
-  //     const userBalance = await boo.balanceOf(selfAddress);
-  //     console.log(1);
-  //     console.log(`userBalance: ${userBalance}`);
-  //     const vaultBalance = await vault.balance();
-  //     console.log("vaultBalance");
-  //     console.log(vaultBalance);
-  //     console.log(2);
-  //     const depositAmount = ethers.utils.parseEther(".1");
-  //     console.log("depositAmount");
-  //     console.log(depositAmount);
-  //     console.log(i);
-  //     await vault.connect(self).deposit(depositAmount);
-  //     console.log(4);
-  //     const newVaultBalance = await vault.balance();
-  //     console.log(`newVaultBalance: ${newVaultBalance}`);
-  //     console.log(`depositAmount: ${depositAmount}`);
-  //     const newUserBalance = await boo.balanceOf(selfAddress);
+  describe("Vault Tests", function () {
+    it("should allow deposits and account for them correctly", async function () {
+      const userBalance = await tarot.balanceOf(selfAddress);
+      console.log(1);
+      console.log(`userBalance: ${userBalance}`);
+      const vaultBalance = await vault.balance();
+      console.log("vaultBalance");
+      console.log(vaultBalance);
+      console.log(2);
+      const depositAmount = ethers.utils.parseEther(".1");
+      console.log("depositAmount");
+      console.log(depositAmount);
+      console.log(3);
+      await vault.connect(self).deposit(depositAmount);
+      console.log(4);
+      const newVaultBalance = await vault.balance();
+      console.log(`newVaultBalance: ${newVaultBalance}`);
+      console.log(`depositAmount: ${depositAmount}`);
+      const newUserBalance = await tarot.balanceOf(selfAddress);
 
-  //     console.log(`newUserBalance: ${newUserBalance}`);
-  //     console.log(
-  //       `userBalance - depositAmount: ${userBalance - depositAmount}`
-  //     );
-  //     console.log(
-  //       `userBalance - newUserBalance: ${userBalance - newUserBalance}`
-  //     );
-  //     const deductedAmount = userBalance.sub(newUserBalance);
-  //     console.log("deductedAmount");
-  //     console.log(deductedAmount);
-  //     const isSmallBalanceDifference = depositAmount.sub(newVaultBalance) < 5;
-  //     expect(vaultBalance).to.equal(0);
-  //     expect(isSmallBalanceDifference).to.equal(true);
-  //     expect(deductedAmount).to.equal(depositAmount);
-  //   });
-  //   it("should mint user their pool share", async function () {
-  //     console.log("---------------------------------------------");
-  //     const userBalance = await boo.balanceOf(selfAddress);
-  //     console.log(userBalance.toString());
-  //     const selfDepositAmount = ethers.utils.parseEther("0.005");
-  //     await vault.connect(self).deposit(selfDepositAmount);
-  //     console.log((await vault.balance()).toString());
+      console.log(`newUserBalance: ${newUserBalance}`);
+      console.log(
+        `userBalance - depositAmount: ${userBalance - depositAmount}`
+      );
+      console.log(
+        `userBalance - newUserBalance: ${userBalance - newUserBalance}`
+      );
+      const deductedAmount = userBalance.sub(newUserBalance);
+      console.log("deductedAmount");
+      console.log(deductedAmount);
+      expect(vaultBalance).to.equal(0);
+      expect(depositAmount).to.equal(newVaultBalance);
+      expect(deductedAmount).to.equal(depositAmount);
+    });
+    xit("should mint user their pool share", async function () {
+      console.log("---------------------------------------------");
+      const userBalance = await tarot.balanceOf(selfAddress);
+      console.log(userBalance.toString());
+      const selfDepositAmount = ethers.utils.parseEther("0.005");
+      await vault.connect(self).deposit(selfDepositAmount);
+      console.log((await vault.balance()).toString());
 
-  //     const whaleDepositAmount = ethers.utils.parseEther("100");
-  //     await vault.connect(booWhale).deposit(whaleDepositAmount);
-  //     console.log((await vault.balance()).toString());
-  //     // console.log((await boo.balanceOf(selfAddress)).toString());
-  //     // const selfBooBalance = await vault.balanceOf(selfAddress);
-  //     // console.log(selfBooBalance.toString());
-  //     const ownerDepositAmount = ethers.utils.parseEther("5");
-  //     await boo.connect(self).transfer(ownerAddress, ownerDepositAmount);
-  //     const ownerBalance = await boo.balanceOf(ownerAddress);
+      const whaleDepositAmount = ethers.utils.parseEther("100");
+      await vault.connect(tarotWhale).deposit(whaleDepositAmount);
+      console.log((await vault.balance()).toString());
+      // console.log((await boo.balanceOf(selfAddress)).toString());
+      // const selfBooBalance = await vault.balanceOf(selfAddress);
+      // console.log(selfBooBalance.toString());
+      const ownerDepositAmount = ethers.utils.parseEther("5");
+      await tarot.connect(self).transfer(ownerAddress, ownerDepositAmount);
+      const ownerBalance = await tarot.balanceOf(ownerAddress);
 
-  //     console.log(ownerBalance.toString());
-  //     await vault.deposit(ownerDepositAmount);
-  //     console.log((await vault.balance()).toString());
-  //     const ownerVaultBooBalance = await vault.balanceOf(ownerAddress);
-  //     console.log(ownerVaultBooBalance.toString());
-  //     await vault.withdraw(ownerVaultBooBalance);
-  //     const ownerBooBalance = await boo.balanceOf(ownerAddress);
-  //     console.log(`ownerBooBalance: ${ownerBooBalance}`);
-  //     const ownerVaultBooBalanceAfterWithdraw = await vault.balanceOf(
-  //       ownerAddress
-  //     );
-  //     console.log(
-  //       `ownerVaultBooBalanceAfterWithdraw: ${ownerVaultBooBalanceAfterWithdraw}`
-  //     );
-  //     // expect(ownerBooBalance).to.equal(ownerDepositAmount);
-  //     // expect(selfBooBalance).to.equal(selfDepositAmount);
-  //   });
-  //   xit("should allow withdrawals", async function () {
-  //     const userBalance = await boo.balanceOf(selfAddress);
-  //     console.log(`userBalance: ${userBalance}`);
-  //     const depositAmount = ethers.BigNumber.from(ethers.utils.parseEther("1"));
-  //     await vault.connect(self).deposit(depositAmount);
-  //     console.log(
-  //       `await boo.balanceOf(selfAddress): ${await boo.balanceOf(selfAddress)}`
-  //     );
-  //     const whaleDepositAmount = ethers.utils.parseEther("100");
-  //     await vault.connect(booWhale).deposit(whaleDepositAmount);
-  //     const newUserBalance = userBalance.sub(depositAmount);
-  //     const tokenBalance = await boo.balanceOf(selfAddress);
-  //     const balanceDifferenceIsZero = tokenBalance.sub(newUserBalance).isZero();
-  //     expect(balanceDifferenceIsZero).to.equal(true);
-  //     await vault.connect(self).withdraw(depositAmount);
-  //     console.log(
-  //       `await boo.balanceOf(selfAddress): ${await boo.balanceOf(selfAddress)}`
-  //     );
-  //     const newUserVaultBalance = await vault.balanceOf(selfAddress);
-  //     console.log(`newUserVaultBalance: ${newUserVaultBalance}`);
-  //     const userBalanceAfterWithdraw = await boo.balanceOf(selfAddress);
-  //     const securityFee = 10;
-  //     const percentDivisor = 10000;
-  //     const withdrawFee = (depositAmount * securityFee) / percentDivisor;
-  //     const expectedBalance = userBalance.sub(withdrawFee);
-  //     const isSmallBalanceDifference =
-  //       expectedBalance.sub(userBalanceAfterWithdraw) < 5;
-  //     expect(isSmallBalanceDifference).to.equal(true);
-  //   });
-  //   xit("should be able to harvest", async function () {
-  //     await strategy.connect(self).harvest();
-  //   });
-  //   xit("should provide yield", async function () {
-  //     await strategy.connect(self).harvest();
-  //     const depositAmount = ethers.utils.parseEther(".05");
-  //     await vault.connect(self).deposit(depositAmount);
-  //     const vaultBalance = await vault.balance();
-  //     console.log(`vaultBalance: ${vaultBalance}`);
-  //     console.log(`depositAmount: ${depositAmount}`);
+      console.log(ownerBalance.toString());
+      await vault.deposit(ownerDepositAmount);
+      console.log((await vault.balance()).toString());
+      const ownerVaultTarotBalance = await vault.balanceOf(ownerAddress);
+      console.log(ownerVaultTarotBalance.toString());
+      await vault.withdraw(ownerVaultTarotBalance);
+      const ownerTarotBalance = await tarot.balanceOf(ownerAddress);
+      console.log(`ownerTarotBalance: ${ownerTarotBalance}`);
+      const ownerVaultTarotBalanceAfterWithdraw = await vault.balanceOf(
+        ownerAddress
+      );
+      console.log(
+        `ownerVaultBooBalanceAfterWithdraw: ${ownerVaultBooBalanceAfterWithdraw}`
+      );
+      // expect(ownerBooBalance).to.equal(ownerDepositAmount);
+      // expect(selfBooBalance).to.equal(selfDepositAmount);
+    });
+    xit("should allow withdrawals", async function () {
+      const userBalance = await boo.balanceOf(selfAddress);
+      console.log(`userBalance: ${userBalance}`);
+      const depositAmount = ethers.BigNumber.from(ethers.utils.parseEther("1"));
+      await vault.connect(self).deposit(depositAmount);
+      console.log(
+        `await tarot.balanceOf(selfAddress): ${await tarot.balanceOf(
+          selfAddress
+        )}`
+      );
+      const whaleDepositAmount = ethers.utils.parseEther("100");
+      await vault.connect(tarotWhale).deposit(whaleDepositAmount);
+      const newUserBalance = userBalance.sub(depositAmount);
+      const tokenBalance = await tarot.balanceOf(selfAddress);
+      const balanceDifferenceIsZero = tokenBalance.sub(newUserBalance).isZero();
+      expect(balanceDifferenceIsZero).to.equal(true);
+      await vault.connect(self).withdraw(depositAmount);
+      console.log(
+        `await tarot.balanceOf(selfAddress): ${await tarot.balanceOf(
+          selfAddress
+        )}`
+      );
+      const newUserVaultBalance = await vault.balanceOf(selfAddress);
+      console.log(`newUserVaultBalance: ${newUserVaultBalance}`);
+      const userBalanceAfterWithdraw = await tarot.balanceOf(selfAddress);
+      const securityFee = 10;
+      const percentDivisor = 10000;
+      const withdrawFee = (depositAmount * securityFee) / percentDivisor;
+      const expectedBalance = userBalance.sub(withdrawFee);
+      const isSmallBalanceDifference =
+        expectedBalance.sub(userBalanceAfterWithdraw) < 5;
+      expect(isSmallBalanceDifference).to.equal(true);
+    });
+    xit("should be able to harvest", async function () {
+      await strategy.connect(self).harvest();
+    });
+    xit("should provide yield", async function () {
+      await strategy.connect(self).harvest();
+      const depositAmount = ethers.utils.parseEther(".05");
+      await vault.connect(self).deposit(depositAmount);
+      const vaultBalance = await vault.balance();
+      console.log(`vaultBalance: ${vaultBalance}`);
+      console.log(`depositAmount: ${depositAmount}`);
 
-  //     await strategy.connect(self).harvest();
-  //     const newVaultBalance = await vault.balance();
-  //     console.log(`newVaultBalance: ${newVaultBalance}`);
-  //     const whaleDepositAmount = ethers.utils.parseEther("4628");
-  //     await vault.connect(booWhale).deposit(whaleDepositAmount);
-  //     const bigWhaleDepositAmount = ethers.utils.parseEther("327171");
-  //     await vault.connect(bigBooWhale).deposit(bigWhaleDepositAmount);
-  //     const minute = 60;
-  //     const hour = 60 * minute;
-  //     const day = 24 * hour;
-  //     await moveTimeForward(10 * day);
-  //     await updatePools(acelab);
-  //     await strategy.connect(self).harvest();
-  //     const newVaultBalance2 = await vault.balance();
-  //     console.log(`newVaultBalance2: ${newVaultBalance2}`);
-  //     const totalDepositAmount = depositAmount
-  //       .add(whaleDepositAmount)
-  //       .add(bigWhaleDepositAmount);
-  //     console.log(`totalDepositAmount: ${totalDepositAmount}`);
-  //     const hasYield = newVaultBalance2 > totalDepositAmount;
-  //     console.log(`hasYield: ${hasYield}`);
-  //     expect(hasYield).to.equal(true);
-  //   });
-  // });
+      await strategy.connect(self).harvest();
+      const newVaultBalance = await vault.balance();
+      console.log(`newVaultBalance: ${newVaultBalance}`);
+      const whaleDepositAmount = ethers.utils.parseEther("4628");
+      await vault.connect(booWhale).deposit(whaleDepositAmount);
+      const bigWhaleDepositAmount = ethers.utils.parseEther("327171");
+      await vault.connect(bigBooWhale).deposit(bigWhaleDepositAmount);
+      const minute = 60;
+      const hour = 60 * minute;
+      const day = 24 * hour;
+      await moveTimeForward(10 * day);
+      await updatePools(acelab);
+      await strategy.connect(self).harvest();
+      const newVaultBalance2 = await vault.balance();
+      console.log(`newVaultBalance2: ${newVaultBalance2}`);
+      const totalDepositAmount = depositAmount
+        .add(whaleDepositAmount)
+        .add(bigWhaleDepositAmount);
+      console.log(`totalDepositAmount: ${totalDepositAmount}`);
+      const hasYield = newVaultBalance2 > totalDepositAmount;
+      console.log(`hasYield: ${hasYield}`);
+      expect(hasYield).to.equal(true);
+    });
+  });
   // describe("Strategy", function () {
   //   xit("should be able to remove a pool", async function () {
   //     await strategy.connect(self).harvest();
